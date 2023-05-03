5242daf353b2eda683977c5dbcae2a6b72c1812c
Elidot14 <74420190+Elidot14@users.noreply.github.com>
2023-05-03 16:01:59 +0200

    Frontend for home page

--- frontend/views/home.php
+++ frontend/views/home.php
@@ -2,14 +2,26 @@
 require_once __DIR__ . "/../Template.php";
 
 Template::header("Home");
+
 ?>
+
+<link href='https://fonts.googleapis.com/css?family=Coiny' rel='stylesheet'>
 
 <div class="home-page">
 
-    <h1>Welcome to Trackersaurus</h1>
+    <h1>Welcome to Trackosaurus</h1>
 
-    <div>
+    <h2>
+    This is Trackosaurus!
+    A tracking application in which parents can track their childrens everyday doings from activities, chores, hobbies, moods, habits, etc. 
+    Every day the child is prompted to record their day with fun and interactive designs by adding different activities and descriptions of their day. 
+    The child can see their recorded activities in the calendar which they can update and delete at any time. 
+    The admin/parent can track their children's activities to get a better insight into their activities, chores, hobbies, moods, habits, etc. in real-time.
+    The children and parents can make separate accounts but each parent is connected with their childs account. 
+    </h2>
 
-    </div>
+    <button>Create account</button>
+
+    <button>Log in </button>
 
 </div>
