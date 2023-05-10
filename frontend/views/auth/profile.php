1956762e2fceb47335a5b219d08346786952d2f0
liislepler <leli19ce@student.ju.se>
2023-05-10 18:06:38 +0200

    update

--- frontend/views/auth/profile.php
+++ frontend/views/auth/profile.php
@@ -9,9 +9,27 @@ Template::header("Profile", $this->model["error"]);
 </p>
 
 <?php if ($this->user->user_role === "parent") : ?>
-    <p>Parent</p>
+    <p>Parent account</p>
     <div>
         <a href="<?= $this->home ?>/auth/add-children">Add your children</a>
+    </div>
+    <div class="item-grid">
+
+        <?php foreach ($this->model as $users) : ?>
+
+            <article class="item">
+                <div>
+                    <b><?= $user->username ?></b> <br>
+                </div>
+                    <p>
+                        <b>User ID: </b>
+                        <?= $purchase->user_id ?>
+                    </p>
+                <a href="<?= $this->home ?>/purchases/<?= $purchase->purchase_id ?>/edit">Edit</a>
+            </article>
+
+        <?php endforeach; ?>
+
     </div>
 <?php endif; ?>
 
